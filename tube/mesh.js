96dcf1981ab4e6201b1e078087d6657a98f45708
CalmSeas <calmseas@cephalon.ai>
2025-09-07 00:29:35 +0800

    feat: 14

---
+++ tube/mesh.js
@@ -0,0 +1,30 @@
+import * as THREE from 'three';
+
+const path = new THREE.CatmullRomCurve3([
+    new THREE.Vector3(-100, 20, 90),
+    new THREE.Vector3(-40, 80, 100),
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(60, -60, 0),
+    new THREE.Vector3(100, -40, 80),
+    new THREE.Vector3(150, 60, 60)
+]);
+
+const geometry = new THREE.TubeGeometry(path, 100, 5, 30);
+
+const texture = new THREE.TextureLoader().load('./image.png');
+texture.wrapS = THREE.RepeatWrapping;
+texture.repeat.x = 20;
+texture.colorSpace = THREE.SRGBColorSpace;
+
+
+const material = new THREE.MeshBasicMaterial({
+   map: texture,
+   aoMap:texture,
+    side: THREE.DoubleSide,
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+
+export const tubePoints =path.getSpacedPoints(1000)
+
+export default mesh;
---
+++ tube/mesh2.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+const p1 = new THREE.Vector3(-100, 0, 0);
+const p2 = new THREE.Vector3(50, 100, 0);
+const p3 = new THREE.Vector3(100, 0, 100);
+const p4 = new THREE.Vector3(100, 0, 0);
+
+const curve = new THREE.CubicBezierCurve3(p1, p2, p3, p4);
+
+const geometry = new THREE.TubeGeometry(curve, 50, 20, 20);
+
+const materail = new THREE.MeshLambertMaterial({
+    color: new THREE.Color('orange'),
+    side: THREE.DoubleSide,
+    wireframe: true
+});
+
+const mesh = new THREE.Mesh(geometry, materail);
+const gui = new GUI();
+
+const obj={
+    TubeSeg:50,
+    radius:20,
+    rediusSeg:20
+}
+
+function onChange(){
+    mesh.geometry = new THREE.TubeGeometry(curve, obj.TubeSeg, obj.radius, obj.rediusSeg);
+}
+gui.add(obj,'TubeSeg').min(10).max(100).step(1).onChange(onChange);
+gui.add(obj,'radius').min(10).max(100).step(1).onChange(onChange);
+gui.add(obj,'rediusSeg').min(10).max(100).step(1).onChange(onChange);
+
+
+const geometry2 = new THREE.BufferGeometry();
+geometry2.setFromPoints([p1,p2,p3,p4]);
+const material2 = new THREE.PointsMaterial({
+    color: new THREE.Color('blue'),
+    size: 10
+});
+const points2 = new THREE.Points(geometry2, material2);
+const line2 = new THREE.Line(geometry2, new THREE.LineBasicMaterial());
+mesh.add(points2, line2);
+
+const point = new THREE.BufferGeometry()
+point.setFromPoints(curve.getPoints(50));
+
+const material3=new THREE.LineBasicMaterial({
+    color: new THREE.Color('red'),
+})
+const line3=new THREE.Line(point,material3);
+
+mesh.add(line3);
+
+
+export default mesh;
